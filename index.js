d19735c1deb250f043fcc77764814eef6c817708
Julien AWONGA <julienawon@gmail.com>
2022-08-13 11:28:45 +0100

    Get api and fix front

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("hello");
+
+var content = document.getElementById("content");
+var author = document.getElementById("author");
+
+async function getQuotes(){
+
+    let url = "https://type.fit/api/quotes";
+
+    try{
+        let query = await fetch(url);
+
+       return await query.json();
+
+    }catch(e){
+        console.log(e);
+    }
+}
+ async function renderQuote(){
+
+    let quotes = await getQuotes();
+
+    let quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+    author.innerHTML = `<a href='https://www.google.com/search?q=${quote.author.split(' ').join('+')}' target='_blank' >${quote.author}<a/>`;
+
+    content.innerHTML = quote.text;
+
+   
+ }
+setInterval(renderQuote, 5000);
+
